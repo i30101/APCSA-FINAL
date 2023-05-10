924d03010f05616ac1c928594553d72faef0e3ca
Andrew Kim <andrewkim229@gmail.com>
2023-05-10 14:54:14 -0400

    python first draft

--- data/graph.py
+++ data/graph.py
@@ -1,53 +1,64 @@
+import getopt
+import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-DAY_PATH = "./data/day-history.csv"
-MONTH_PATH = "./data/month-history.csv"
-YEAR_PATH = "./data/year-history.csv"
-X_AXIS = list(i for i in range(1, 102))
-
-df = pd.read_csv(DAY_PATH)
-
-
-tickers = list(df.iloc[:, 0])
-print(tickers)
-
-def graph_day_ticker(ticker: str):
-    if(ticker in tickers):
-        i = tickers.index(ticker)
-        row = list(df.iloc[i])
-        ticker = row[0]
-        data = row[1:]
-        
-        line_graph(f"Day Price of {ticker}", "Price ($)", data)
+# initialize data
+dataframes = {
+    "day" : pd.read_csv("./data/day-history.csv"),
+    "month" : pd.read_csv("./data/month-history.csv"),
+    "year" : pd.read_csv("./data/year-history.csv")
+}
+keys = list(dataframes.keys())
+tickers = list(dataframes["day"]["Ticker"].values)
 
 
-def line_graph(title, y_label, data):
+# extract data for desired ticker
+def graph_ticker_history(ticker, index, frame: str):
+    dataframe = dataframes[frame]
+    row = list(dataframe.iloc[index])[1:]
+    title = f"{frame.capitalize()} Price of {ticker}"
+    closing_price = str(row[-1])
+    if closing_price[-2 : -1] == ".":
+        closing_price += "0"
+    x_label = f"Current Price: ${closing_price}"
+    y_label = "Price ($)"
+
+    line_graph(title, x_label, y_label, row)
+
+    
+# create a line graph 
+def line_graph(title, x_label, y_label, data):
+    length = len(data)
+    
     # determine graph color
     col = "green" if data[0] < data[-1] else "red"
     
-    # label graph
-    # plt.title(title)
-    # plt.ylabel(y_label)
-    
-    # fig, ax = plt.subplots(figsize = (6, 6))
-    # plt.show()
-    
-    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    # size figure
+    plt.rcParams["figure.figsize"] = [7, 5]
     plt.rcParams["figure.autolayout"] = True
-    X = np.linspace(0, 101, 101, endpoint=True)
+    
+    X = np.linspace(0, length, length, endpoint=True)
     fig, ax = plt.subplots()
+    
+    # label graph
     ax.set_title(title)
+    closing_price = str(data[-1])
+    if closing_price[-2 : -1] == ".":
+        closing_price += "0"
+    ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
+    
+    # set axes limits
+    ax.set_xlim(0, length)
+    ax.set_ylim(min(data) * 0.95, max(data) * 1.01)
+    
     ax.plot(X, data, color=col, alpha=1.00)
     ax.fill_between(X, data, 0, color=col, alpha=.1)
+    
     plt.show()
     
-    
-# graph_day_ticker("INTC")
-graph_day_ticker("AXP")
-# graph_day_ticker("AMGN")
 
-
+print(sys.argv)
